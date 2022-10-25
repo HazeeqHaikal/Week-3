b3ab8933d7e1ef318d36a51d851141423b550c06
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-10-25 10:49:47 +0800

    updated sphere volume

--- Problem 6/sphereVolume.cpp
+++ Problem 6/sphereVolume.cpp
@@ -1,18 +1,29 @@
+#include <cmath>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
+#define PI 3.14159265358979323846
+// const double PI = 3.14159;
+const char newline = '\n';
+
+int main() {
+    string name;
     double radius, volume;
-    const double PI = 3.14159;
+
+    cout << "Enter your name: ";
+    // input long name
+    getline(cin, name);
+
+    cout << "Welcome " << name << newline;
 
     cout << "Enter the radius of the sphere: ";
     cin >> radius;
 
-    volume = (4.0 / 3.0) * PI * radius * radius * radius;
+    volume = (4.0 / 3.0) * PI * pow(radius, 3);
 
-    cout << "The volume of the sphere is " << volume << endl;
+    cout << fixed << "The volume of the sphere is " << volume << endl;
 
     return 0;
 }
