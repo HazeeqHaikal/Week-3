e3af110ee4b0130cba46c036635eb3f690873624
Hazeeq Haikal <43111765+HazeeqHaikal@users.noreply.github.com>
2022-10-25 11:16:35 +0800

    updated tax

--- Problem 7/tax.cpp
+++ Problem 7/tax.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <iostream>
 
 using namespace std;
@@ -6,11 +7,11 @@ int main() {
     float tax_rate = 0.06;
     float total, total_after_tax;
 
-    // cout << "Enter the total: ";
+    cout << "Enter the total: ";
     cin >> total;
 
     total_after_tax = total + (total * tax_rate);
 
-    cout << total_after_tax;
-    // cout << "Total after tax: " << total_after_tax << endl;
+    cout << setprecision(2) << fixed;
+    cout << "Total after tax: RM" << total_after_tax << endl;
 }
